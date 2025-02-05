2127aaf2ed409b29ef30bb1eb992f6acb950861a
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-05 08:37:12 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const counterElement = document.getElementById("counter");
+let count = +counterElement.innerText;
+const increment = () => {
+    count++;
+    counterElement.innerText = count.toString();
+};
+
+const incrementButton = document.getElementById("increment");
+incrementButton.addEventListener("click", increment);
+
+// TODO: You do for decrement.
